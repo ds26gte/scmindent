0af719b026b59a431a30c42742a1b6262d43e995
Dorai Sitaram <ds26gte@yahoo.com>
2014-09-16 21:54:19 -0400

    Initial commit of scmindent.js, which indents Lisp code in stdin to stdout.

---
+++ scmindent.js
@@ -0,0 +1,196 @@
+#!/usr/bin/env node
+
+// Dorai Sitaram
+// last modified 2014-09-16
+
+// this script takes lines of Lisp or Scheme code from its
+// stdin and produces an indented version thereof on its
+// stdout
+
+var readline = require('readline');
+var fs = require('fs');
+
+var lispKeywords = {};
+
+function numLeadingSpaces(s) {
+  if (s.match(/^\s*$/)) {
+    return 0;
+  } else {
+    s = s.replace(/^(\s*).*/, '$1');
+    s = s.replace(/\t/g, '        ');
+    return s.length;
+  }
+}
+
+function stringTrimBlanks(s) {
+  s = s.replace(/^\s*/, '');
+  s = s.replace(/\s*$/, '');
+  return s;
+}
+
+function literalTokenP(s) {
+  return !!s.match(/^[0-9#"]/);
+}
+
+function lispIndentNumber(s) {
+  var n;
+  s = s.toLowerCase();
+  if (n = lispKeywords[s]) {
+    return n;
+  } else if (s.match(/^def/)) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
+
+function pastNextToken(s, i) {
+  var n = s.length;
+  var escapeP = false;
+  while (true) {
+    if (i >= n) {
+      return n;
+    } else {
+      var c = s[i];
+      if (escapeP) {
+        escapeP = false; i++;
+      } else if (c === '\\') {
+        escapeP = true; i++;
+      } else if (c === '#') {
+        if (s[i+1] === '\\') {
+          escapeP = true; i = i+2;
+        } else {
+          return i;
+        }
+      } else if (c.match(/[ \t\(\)\[\]"'`,;]/)) {
+        return i;
+      } else {
+        escapeP = false; i++;
+      }
+    }
+  }
+}
+
+function calcSubindent(s, i) {
+  var n = s.length;
+  var j = pastNextToken(s, i);
+  var numAlignedSubforms = 0;
+  var leftIndent;
+  if (j === i) {
+    leftIndent = 1;
+  } else {
+    var w = s.substring(i, j);
+    if (i >= 2 && s[i-2].match(/['`]/)) {
+      leftIndent = 1;
+    } else {
+      var nas = lispIndentNumber(w);
+      if (nas >= 0) {
+        numAlignedSubforms = nas;
+        leftIndent = 2;
+      } else if (literalTokenP(w)) {
+        leftIndent = 1;
+      } else if (j === n) {
+        leftIndent = 1;
+      } else {
+        leftIndent = j - i + 2;
+      }
+    }
+  }
+  return [ leftIndent,
+  numAlignedSubforms,
+  j-1 ];
+}
+
+var leftI = 0;
+var parenStack = [];
+var stringP = false;
+
+function indentLine(currLine) {
+  var leadingSpaces = numLeadingSpaces(currLine);
+  var currLeftI;
+  if (stringP) {
+    currLeftI = leadingSpaces;
+  } else if (parenStack.length === 0) {
+    if (leftI === 0) { leftI = leadingSpaces; }
+    currLeftI = leftI;
+  } else {
+    currLeftI = parenStack[0].spacesBefore;
+    var extraW = 0;
+    if (parenStack[0].numFinishedSubforms < parenStack[0].numAlignedSubforms) {
+      parenStack[0].numFinishedSubforms++;
+      currLeftI += 2;
+    }
+  }
+  currLine = stringTrimBlanks(currLine);
+  for (var i = 0; i < currLeftI; i++) {
+    process.stdout.write(' ');
+  }
+  process.stdout.write(currLine);
+  process.stdout.write('\n');
+
+  var escapeP = false;
+  var interWordSpaceP = false;
+  var i = 0;
+  while (i < currLine.length) {
+    var c = currLine[i];
+    if (escapeP) {
+      escapeP = false; i++;
+    } else if (c === '\\') {
+      escapeP = true; i++;
+    } else if (stringP) {
+      if (c === '"') { stringP = false; } i++;
+    } else if (c === ';') {
+      break;
+    } else if (c === '"') {
+      stringP = true; i++;
+    } else if (c.match(/[ \t]/)) {
+      if (!interWordSpaceP) {
+        interWordSpaceP = true;
+        if (parenStack.length > 0) {
+          parenStack[0].numFinishedSubforms++;
+        }
+      }
+      i++;
+    } else if (c.match(/[\(\[]/)) {
+      interWordSpaceP = false;
+      var si = calcSubindent(currLine, i+1);
+      parenStack.unshift({ spacesBefore: i + currLeftI + si[0],
+        numAlignedSubforms: si[1],
+        numFinishedSubforms: 0
+      });
+      i = si[2] + 1;
+    } else if (c.match(/[\)\]]/)) {
+      interWordSpaceP = false;
+      if (parenStack.length > 0) {
+        parenStack.shift();
+        leftI = 0;
+      }
+      i++;
+    } else {
+      interWordSpaceP = false; i++;
+    }
+  }
+}
+
+fs.readFile(process.env.HOME + '/lispwords.json', 'utf8',
+    function(err, data) {
+      if (!err) {
+        var lw = JSON.parse(data);
+        for (var ino in lw) {
+          var kww = lw[ino];
+          for (var i = 0; i < kww.length; i++) {
+            var kw = kww[i].toLowerCase();
+            lispKeywords[kw] = ino;
+          }
+        }
+        var rl = readline.createInterface({
+          input: process.stdin,
+          output: process.stdout,
+          terminal: false
+        });
+        rl.on('line', indentLine);
+        rl.on('close', function() {});
+      } else {
+        //ignore error
+      }
+    });
